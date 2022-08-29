6e9512226106bed4839347489cb182a50fc969d1
rizkytsun <rizkytsun@gmail.com>
2022-08-29 22:27:20 +0700

    HTTP REQUEST

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,61 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/json', function () {
+    return Response()->json([
+        'message' => 'Hello, Laravel!'
+    ]);
+});
+
+//DATA ARRAY
+Route::get('json2', function () {
+    $DataArray = [
+        'message' => 'Hello, Laravel2!'
+    ];
+    return Response()->json($DataArray, 404);
+});
+
+// DEBUG
+Route::get('debug', function () {
+    $DataArray = [
+        'message' => 'Hello, Laravel2!'
+    ];
+    dd($DataArray);
+});
+
+// FUNGSI HELPER 
+Route::get('debug_helper', function () {
+    $DataArray = [
+        'message' => 'Hello, Laravel2!'
+    ];
+    dd(request());
+});
+
+// DEBUG FORMAT LEBIH RAPI
+Route::get('debug_helper2', function () {
+    $DataArray = [
+        'message' => 'Hello, Laravel2!'
+    ];
+    // d 3x
+    ddd($DataArray);
+});
+
+// MEMBUAT DATA ARRAY UNTUK DI TAMPILKAN DI POSTMAN
+$taskList = [
+    'first' => 'Sleep',
+    'two' => 'Eat',
+    'three' => 'Work'
+];
+
+Route::get('/tasks', function() use($taskList) {
+    return $taskList;
+});
+
+Route::get('/tasks/{param}', function($param) use ($taskList) {
+    return $taskList[$param];
+});
